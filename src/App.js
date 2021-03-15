a4a782618bd834da68b49c7c69fbf7190d2222c4
Sangmin Lee <tkdals198@naver.com>
2021-03-15 14:36:30 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
+import Table from  '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+import { Paper } from '@material-ui/core';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 1080,
+  }
+});
+
 
 const customers = [
   {
@@ -28,26 +47,27 @@ const customers = [
   'job' : '대학생',
 }
 ]
-function App() {
+function App(props) {
+  const { classes } = props;
   return (
-    <div>
-      {
-        customers.map(c => {
-          return (
-            <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          );
-        })
-      }
-    </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map(c => <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)}  
+        </TableBody> 
+      </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
